23f654dd9b5067401c0ca80ec553ae76dea4b1eb
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-10 12:54:41 +0530

    examples

--- src/currency/curlib.go
+++ src/currency/curlib.go
@@ -22,3 +22,30 @@ type CurrencyError struct {
    Error string `json:"currency_error"`
 }
 
+func Load(path string) []Currency {
+   table := make([]Currency, 0)
+   file, err := os.Open(path)
+   if err != nil {
+      panic(err.Error())
+   }
+   defer file.Close()
+
+   reader := csv.NewReader(file)
+   for {
+      row, err := reader.Read()
+      if err == io.EOF {
+         break
+      }
+      if err != nil {
+         panic(err.Error())
+      }
+      c := Currency{
+         Country: row[0],
+         Name:    row[1],
+         Code:    row[2],
+         Number:  row[3],
+      }
+      table = append(table, c)
+   }
+   return table
+}
