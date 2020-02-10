844daef9a5363fbc14f2ed8055be4c56f4a58bbd
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-10 13:23:52 +0530

    examples

--- src/currency/echos.go
+++ src/currency/echos.go
@@ -47,3 +47,27 @@ func main() {
       go handleConnection(conn)
    }
 }
+
+// handleConnection reads request from connection
+// with conn.Read() then write response using
+// conn.Write(). Then connection is closed.
+
+func handleConnection(conn *net.TCPConn) {
+   defer conn.Close()
+   buf := make([]byte, 1024)
+   n, err := conn.Read(buf)
+   if err != nil {
+      fmt.Println(err)
+      return
+   }
+
+   w, err := conn.Write(buf[:n])
+   if err != nil {
+      fmt.Println("failed to write to client:", err)
+      return
+   }
+   if w != n {
+      fmt.Println("warning: not all data sent to client")
+      return
+   }
+}
