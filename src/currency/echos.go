15c3e7c40022d7d4929d18b639940cfb5f5efb28
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-10 13:18:28 +0530

    examples

---
+++ src/currency/echos.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+   "flag"
+   "fmt"
+   "net"
+   "os"
+)
+
+// This program implements a simple echo server over TCP.
+// When server recieves a request, it returns its content
+// immediately
+
+func main() {
+   var addr string
+   flag.StringVar(&addr, "e", ":4040", "service address endpoint")
+   flage.Parse()
+   
+   // create local addr for socket
+   laddr, err := netResolveTCPAddr("tcp", addr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   
+   // announce service using ListenTCP
+   // which a TCPListener.
+   l, err := net.ListenTCP("tcp", laddr)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   defer l.Close()
+   fmt.Println("listening at (tcp)", laddr.String())
+   
+   // req/response loop
+   for {
+      // use TCPListener to block and wait for TCP
+      // connection request using AcceptTCP which creates a TCPConn
+      conn, err := l.AcceptTCP()
+      if err != nil {
+         fmt.Println("failed to accept conn:", err)
+         conn.Close()
+         continue
+      }
+      fmt.Println("connected to: ", conn.RemoteAddr())
+      go handleConnection(conn)
+   }
+}
