a1f2a1aedbb2ce998e7c99b96f19d8d0cc4a7963
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-10 14:05:03 +0530

    examples

---
+++ src/currency/currs.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+   "flag"
+   "fmt"
+   "log"
+   "net"
+   "strings"
+   curr "currency"
+)
+
+var currencies = curr.Load("../data.csv")
+
+func main() {
+   var addr string
+   var network string
+   flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+   flag.StringVar(&network, "n", "tcp", "network protocol [tcp, unix]")
+   flag.Parse()
+   
+   switch network {
+   case "tcp", "tcp4", "tcp6", "unix":
+   default:
+      log.Fatalln("unsupported network protocol:", network)
+   }
+   
+   // create a listener for provided network and host address
+   ln, err := net.Listen(network, addr)
+   if err != nil {
+      log.Fatal("failed to create listener:", err)
+   }
+   defer ln.Close()
+   log.Println("***** Global Currency Service *****")
+   log.Printf("Service started : (%s) %s\n", network, addr)
+   
+   for {
+      conn, err := ln.Accept()
+      if err != nil {
+         fmt.Println(err)
+         if err := conn.Close(); err != nil {
+            log.Println("failed to close listener:", err)
+         }
+         continue
+      }
+      log.Println("Connected to", conn.RemoteAddr())
+      go handleConnection(conn)
+   }
+}
