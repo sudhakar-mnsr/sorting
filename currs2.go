da606dedf794361da3a2116f55b00d318d3f6b5f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-11 12:01:16 +0530

    examples

--- currs2.go
+++ currs2.go
@@ -13,15 +13,39 @@ import (
 var currencies = curr.Load("../data.csv")
 
 func main() {
-var addr string
-var network string
-flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or sock path]")
-flag.StringVar(&network, "n", "tcp", "network protocol [tcp or unix]")
-flag.Parse()
-
-// validate supported network protocols
-switch network {
-case "tcp","tcp4","tcp6","unix":
-default:
-   log.Fatalln("Unsupported network protocol:", network)
+   var addr string
+   var network string
+   flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or sock path]")
+   flag.StringVar(&network, "n", "tcp", "network protocol [tcp or unix]")
+   flag.Parse()
+   
+   // validate supported network protocols
+   switch network {
+   case "tcp","tcp4","tcp6","unix":
+   default:
+      log.Fatalln("Unsupported network protocol:", network)
+   }
+   
+   // create a listener for provided network and host address
+   ln, err := net.Listen(network, addr)
+   if err != nil {
+      log.Fatal("failed to create listener:", err)
+   }
+   defer ln.Close()
+   log.Println("***** Global Currency Service *****")
+   log.Printf("Service started: (%s) %s\n", network, addr)
+   
+   // connection loop handle incoming requests
+   for {
+      conn, err := ln.Accept()
+      if err != nil {
+         fmt.Println(err)
+         if err := conn.Close(); err != nil {
+            log.Println("failed to close listener:", err)
+         }
+         continue
+      }
+      log.Println("Connected to", conn.RemoteAddr())
+      go handleConnection(conn)
+   }
 }
