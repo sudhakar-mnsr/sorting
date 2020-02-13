ad351761a9149b44add6f4f1b3908e4c0701a8c6
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-13 23:20:23 +0530

    examples

--- io/lowlevel.go
+++ io/lowlevel.go
@@ -4,3 +4,19 @@ import (
     "syscall"
     "fmt"
 )
+
+func main() {
+    disk := "/dev/sda"
+    var fd, numread int
+    var err error
+
+    fd, err = syscall.Open(disk, syscall.O_RDONLY, 0777)
+
+    if err != nil {
+        fmt.Print(err.Error(), "\n")
+        return
+    }
+
+    buffer := make([]byte, 10, 100)
+
+    numread, err = syscall.Read(fd, buffer)
