7d49393f202de886950ff6d22d7c27c94c8537fe
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-14 13:11:39 +0530

    examples

--- io/writer3.go
+++ io/writer3.go
@@ -15,3 +15,18 @@ func NewChannelWriter() *channelWriter {
 		Channel: make(chan byte, 1024),
 	}
 }
+
+func (c *channelWriter) Write(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	go func() {
+		defer close(c.Channel)
+		for _, b := range p {
+			c.Channel <- b
+		}
+	}()
+
+	return len(p), nil
+}
