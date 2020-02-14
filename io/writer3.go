64b47c44fa46386562d2b8c52cd1ed10fea21f2a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-14 13:10:48 +0530

    examples

---
+++ io/writer3.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type channelWriter struct {
+	Channel chan byte
+}
+
+func NewChannelWriter() *channelWriter {
+	return &channelWriter{
+		Channel: make(chan byte, 1024),
+	}
+}
