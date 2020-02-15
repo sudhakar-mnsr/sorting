06d2c4bef6a3da4b239a8c4345c47d4b7fe2cd9e
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-15 23:05:11 +0530

    examples

--- io/bufscan0.go
+++ io/bufscan0.go
@@ -14,3 +14,18 @@ func main() {
 		return
 	}
 	defer file.Close()
+
+	fmt.Printf(
+		"%-10s %-10s %-6s %-6s\n",
+		"Planet", "Diameter", "Moons", "Ring?",
+	)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		fields := strings.Split(scanner.Text(), " ")
+		fmt.Printf(
+			"%-10s %-10s %-6s %-6s\n",
+			fields[0], fields[1], fields[2], fields[3],
+		)
+	}
+}
