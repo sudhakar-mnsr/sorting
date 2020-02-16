94e761a34fdaed82a3fe9ea579d3039c375db728
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-16 23:18:46 +0530

    examples

--- io/gob1.go
+++ io/gob1.go
@@ -19,3 +19,14 @@ type Book struct {
 	Publisher   string
 	PublishDate time.Time
 }
+
+func main() {
+	books := []Book{
+		Book{
+			Title:       "Leaning Go",
+			PageCount:   375,
+			ISBN:        "9781784395438",
+			Authors:     []Name{{"Vladimir", "Vivien"}},
+			Publisher:   "Packt",
+			PublishDate: time.Date(2016, time.July, 0, 0, 0, 0, 0, time.UTC),
+		},
