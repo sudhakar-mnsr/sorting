75035cba8c686c796e8d4aee5963c117868f5694
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-16 23:20:09 +0530

    examples

--- io/gob1.go
+++ io/gob1.go
@@ -47,3 +47,8 @@ func main() {
 			PublishDate: time.Date(2016, time.January, 0, 0, 0, 0, 0, time.UTC),
 		},
 	}
+	file, err := os.Create("book.dat")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
