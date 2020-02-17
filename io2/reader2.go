5dcbdab084f073b5a4a89e5c114e5f632515699e
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-17 13:04:13 +0530

    examples

---
+++ io2/reader2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+   "io"
+   "os"
+   "fmt"
+   "strings"
+)
+
+type alphaReader struct {
+   src io.Reader
+}
+
+func NewAlphaReader(source io.Reader) *alphaReader {
+   return &alphaReader{source}
+}
+
+func (a *alphaReader) Read(p []byte) (int, error) {
+   if len(p) == 0 {
+      return 0, nil
+   }
+   count, err := a.src.Read(p)
+   if err != nil {
+      return count, err
+   }
+   for i := 0; i < len(p); i++ {
+      if (p[i] >= 'A' && p[i] <= 'Z') || (p[i] >= 'a' && p[i] <= 'z') {    
+         continue
+      } else {
+         p[i] = 0
+      }
+   }
+   return count, io.EOF
+}
+
+func main() {
+   str := strings.NewReader("Hello! Where is the sun?")
+   alpha := NewAlphaReader(str)
+   io.Copy(os.Stdout, str)
+   io.Copy(os.Stdout, alpha)
+   fmt.Println()
+}
