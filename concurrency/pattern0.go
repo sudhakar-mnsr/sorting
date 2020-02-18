7d3f0404fd24ecb03af55565f8214f1b92de9715
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-18 14:30:13 +0530

    examples

--- concurrency/pattern0.go
+++ concurrency/pattern0.go
@@ -6,24 +6,29 @@ import (
 )
 
 func main() {
-data := []string{
-   "The yellow fish swims slowly in the water",
-   "The brown dog barks loudly after a drink from its water bowl",
-   "The dark bird of prey lands on a small tree after hunting for fish",
-}
-
-histogram := make(map[string]int)
-done := make(chan bool)
-
-go func() {
-   for _, line := range data {
-      words := strings.Split(line, " ")
-      for _, word := range words {
-         word = strings.ToLower(word)
-         histogram[word]++
+   data := []string{
+      "The yellow fish swims slowly in the water",
+      "The brown dog barks loudly after a drink from its water bowl",
+      "The dark bird of prey lands on a small tree after hunting for fish",
+   }
+   
+   histogram := make(map[string]int)
+   done := make(chan bool)
+   
+   go func() {
+      for _, line := range data {
+         words := strings.Split(line, " ")
+         for _, word := range words {
+            word = strings.ToLower(word)
+            histogram[word]++
+         }
+      }
+      done <- true
+   }() 
+   
+   if <-done {
+      for k, v := range histogram {
+         fmt.Printf("%s\t(%d)\n", k, v)
       }
    }
-   done <- true
-}() 
-
-
+}
