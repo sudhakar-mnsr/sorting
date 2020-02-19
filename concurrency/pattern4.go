3c1cb2fb77764b30232675e1b26bba2c8815cda5
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-19 10:49:50 +0530

    examples

--- concurrency/pattern4.go
+++ concurrency/pattern4.go
@@ -6,21 +6,37 @@ import (
 )
 
 func main() {
-data := []string{
-   "The yellow fish swims slowly in the water",
-   "The brown dog barks loudly after a drink from its water bowl",
-   "The dark bird of prey lands on a small tree after hunting for fish",
+   data := []string{
+      "The yellow fish swims slowly in the water",
+      "The brown dog barks loudly after a drink from its water bowl",
+      "The dark bird of prey lands on a small tree after hunting for fish",
+   }
+   
+   histogram := make(map[string]int)
+   
+   words := words(data)
+   for word := range words {
+      histogram[word]++
+   }
+   
+   for k, v := range histogram {
+      fmt.Println("%s\t(%d)\n", k, v)
+   }
 }
 
-histogram := make(map[string]int)
-
-words := words(data)
-for word := range words {
-   histogram[word]++
-}
-
-for k, v := range histogram {
-   fmt.Println("%s\t(%d)\n", k, v)
-}
+// generator function that produces data
+func words(data []string) <-chan string {
+   out := make(chan string)
+   go func() {
+      defer close(out)
+      for _, line := range data {
+         words := strings.Split(line, " ")
+         for _, word := range words {
+            word = strings.ToLower(word)
+            out <- word
+         }
+      }
+   }()
+   return out
 }
 
