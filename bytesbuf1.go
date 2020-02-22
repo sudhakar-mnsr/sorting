d191aa18965f684d10bc5f29893c283a66056bd1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-22 21:35:55 +0530

    examples

--- bytesbuf1.go
+++ bytesbuf1.go
@@ -10,7 +10,7 @@ func main() {
    var books bytes.Buffer
    books.WriteString("The Great Gatsby\n")
    books.WriteString("1984\n")
-   books.WriteStrin("A Take of Two Cities\n")
+   books.WriteString("A Take of Two Cities\n")
    books.WriteString("Les Miserables\n")
    books.WriteString("The Call of the Wild\n")
 
