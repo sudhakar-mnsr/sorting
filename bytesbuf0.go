46384fc23d1d8d4a747d33ea1c5ca5ca76efc016
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-22 21:30:59 +0530

    examples

---
+++ bytesbuf0.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+   "bytes"
+   "os"
+)
+
+func main() {
+   var books bytes.Buffer
+   books.WriteString("The Great Gatsby")
+   books.WriteString("1984")
+   books.WriteString("A Tale of Two Cities")
+   books.WriteString("Les Miserables")
+   books.WriteString("The Call of the Wild")
+
+   books.WriteTo(os.Stdout)
+}
+
