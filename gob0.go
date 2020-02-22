446c7511320e12fd5e9eeb12aa4ea3f651174b80
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-22 21:48:37 +0530

    examples

--- gob0.go
+++ gob0.go
@@ -19,3 +19,31 @@ type Book struct {
 	Publisher   string
 	PublishDate time.Time
 }
+
+func main() {
+	books := []Book{
+		Book{
+			Title:       "Leaning Go",
+			PageCount:   375,
+			ISBN:        "9781784395438",
+			Authors:     []Name{{"Vladimir", "Vivien"}},
+			Publisher:   "Packt",
+			PublishDate: time.Date(2016, time.July, 0, 0, 0, 0, 0, time.UTC),
+		},
+		Book{
+			Title:       "The Go Programming Language",
+			PageCount:   380,
+			ISBN:        "9780134190440",
+			Authors:     []Name{{"Alan", "Donavan"}, {"Brian", "Kernighan"}},
+			Publisher:   "Addison-Wesley",
+			PublishDate: time.Date(2015, time.October, 26, 0, 0, 0, 0, time.UTC),
+		},
+		Book{
+			Title:       "Introducing Go",
+			PageCount:   124,
+			ISBN:        "978-1491941959",
+			Authors:     []Name{{"Caleb", "Doxsey"}},
+			Publisher:   "O'Reilly",
+			PublishDate: time.Date(2016, time.January, 0, 0, 0, 0, 0, time.UTC),
+		},
+	}
