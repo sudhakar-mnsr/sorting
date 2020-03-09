9916934d49e81d882e1bab3878a64e2691665705
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-09 13:38:28 +0530

    examples

--- asn1.go
+++ asn1.go
@@ -18,3 +18,10 @@ func main() {
 
    fmt.Println("After marshal/unmarshal: ", n)
 }
+
+func checkError(err error) {
+   if err != nil {
+      fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+      os.Exit(1)
+   }
+}
