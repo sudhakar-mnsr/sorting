141c505b2d226924cebf901747d990d3c067e490
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-09 14:27:15 +0530

    examples

--- asnDaytimeClient.go
+++ asnDaytimeClient.go
@@ -10,4 +10,16 @@ import (
         "time"
 )
 
+func main() {
+   if len(os.Args) != 2 {
+      fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
+      os.Exit(1)
+   }
+   service := os.Args[1]
+
+   conn, err := net.Dial("tcp", service)
+   checkError(err)
+
+   result, err := readFully(conn)
+   checkError(err)
 
