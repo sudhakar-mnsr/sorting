80a76fa823e39dc3056d14f70439691511f67a27
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-09 14:22:01 +0530

    examples

--- asnDaytimeServer.go
+++ asnDaytimeServer.go
@@ -15,3 +15,16 @@ func main() {
 
    listener, err := net.ListenTCP("tcp", tcpAddr)
    checkError(err)
+   for {
+      conn, err := listener.Accept()
+      if err != nil {
+         continue
+      }
+   
+      daytime := time.Now()
+      mdata, _ := asn1.Marshal(daytime)
+      conn.Write(mdata)
+      conn.Close()
+   } 
+}
+
