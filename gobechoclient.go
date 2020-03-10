557afd71953e77c487fd57e14871aa77e7ac787a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-10 17:54:21 +0530

    examples

--- gobechoclient.go
+++ gobechoclient.go
@@ -33,41 +33,6 @@ func (p Person) String() string {
 }
 
 func main() {
-package main
-
-import (
-        "bytes"
-        "encoding/gob"
-        "fmt"
-        "io"
-        "net"
-        "os"
-)
-
-type Person struct {
-        Name  Name
-        Email []Email
-}
-
-type Name struct {
-        Family   string
-        Personal string
-}
-
-type Email struct {
-        Kind    string
-        Address string                                                                  
-}
-
-func (p Person) String() string {
-        s := p.Name.Personal + " " + p.Name.Family
-        for _, v := range p.Email {
-                s += "\n" + v.Kind + ": " + v.Address
-        }
-        return s
-}
-
-func main() {
    clientPerson := Person{
       Name: Name{Family: "Client", Personal: "Jan"},
       Email: []Email{Email{Kind: "home", Address: "jan@client.name"},
