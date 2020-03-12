4904b54bead9031a3d29db2fd3ee7514c0ea7cbc
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-12 15:58:33 +0530

    examples

---
+++ udpdaytimeserver.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+   "fmt"
+   "net"
+   "os"
+   "time"
+)
+
+func main() {
+   service := ":1200"
+   udpAddr, err := net.ResolveUDPAddr("udp", service)
+   checkError(err)
+
+   conn, err := net.ListenUDP("udp", udpAddr)
+   checkError(err)
+
+   for {
+      handleClient(conn)
+   }
+}
+
+
