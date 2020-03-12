c261a70dfe238c86a82a238aef7202cb2c6cfeb3
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-12 19:54:53 +0530

    examples

--- simpleechoserver.go
+++ simpleechoserver.go
@@ -20,3 +20,25 @@ func main() {
       conn.Close()
    }
 }    
+
+func handleClient(conn net.Conn) {
+   var buf [512]byte
+   for {
+      n, err := conn.Read(buf[0:])
+      if err != nil {
+         return
+      }
+      fmt.Println(string(buf[0:])
+      _, err2 := conn.Write(buf[0:n])
+      if err2 != nil {
+         return
+      }
+   }
+}
+
+func checkError(err error) {
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+                os.Exit(1)
+        }
+} 
