3af3c6e092937a9f362b6d9912ba415b3d6a15c1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-12 16:01:28 +0530

    examples

--- udpdaytimeserver.go
+++ udpdaytimeserver.go
@@ -20,4 +20,21 @@ func main() {
    }
 }
 
+func handleClient(conn *net.UDPConn) {
+   var buf [512]byte
 
+   _, addr, err := conn.ReadFromUDP(buf[0:])
+   if err != nil {
+      return
+   }
+   daytime := time.Now().String()
+
+   conn.WriteToUDP([]byte(daytime), addr)
+}
+
+func checkError(err error) {
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
+                os.Exit(1)
+        }
+}
