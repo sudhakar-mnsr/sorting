0304a7fb547f7a6de8e31ccc252afa7b14c6b44d
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-12 20:21:41 +0530

    examples

--- ipgetheadinfo1.go
+++ ipgetheadinfo1.go
@@ -27,3 +27,28 @@ func main() {
    fmt.Println(string(result))
    os.Exit(0)
 } 
+
+func checkError(err error) {
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+                os.Exit(1)
+        }
+}
+
+func readFully(conn net.Conn) ([]byte, error) {
+        defer conn.Close()
+
+        result := bytes.NewBuffer(nil)
+        var buf [512]byte
+        for {
+                n, err := conn.Read(buf[0:])
+                result.Write(buf[0:n])
+                if err != nil {
+                        if err == io.EOF {
+                                break
+                        }
+                        return nil, err
+                }
+        }
+        return result.Bytes(), nil                                          
+}
