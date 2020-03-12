10767ea56e16539f5d445103c74b9ac40bb4169b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-12 19:44:53 +0530

    examples

--- tcpdaytimeserver1.go
+++ tcpdaytimeserver1.go
@@ -22,3 +22,26 @@ func main() {
       go handleClient(conn)
    }
 }
+
+func handleClient(conn net.Conn) {
+   defer conn.Close()
+   var buf [512]byte
+   for {
+      n, err := conn.Read(buf[0:]))
+      if err != nil {
+         return
+      }
+      fmt.Println(string(buf[0:]))
+      _, err2 := conn.Write(buf[0:n])
+      if err2 != nil {
+         return
+      }
+   }
+}
+
+func checkError(err error) {
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+                os.Exit(1)
+        }                                                        
+}
