ef3fb1f0494bcf511ea389865a93e2ee4e56adf4
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-12 20:08:40 +0530

    examples

--- threadechoserver1.go
+++ threadechoserver1.go
@@ -20,3 +20,26 @@ func main() {
    }
 }
 
+func handleClient(conn net.Conn) {
+        defer conn.Close()
+
+        var buf [512]byte
+        for {
+                n, err := conn.Read(buf[0:])
+                if err != nil {
+                        return
+                }
+                _, err2 := conn.Write(buf[0:n])
+                if err2 != nil {
+                        return
+                }
+        }
+}
+
+func checkError(err error) {
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+                os.Exit(1)
+        }
+}
+
