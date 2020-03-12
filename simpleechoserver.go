296875ac7706eedbaa4e05a324a093b772e48cc3
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-12 19:49:27 +0530

    examples

---
+++ simpleechoserver.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+        "fmt"
+        "net"
+        "os"
+)
+
+func main() {
+   service := ":1201"
+   tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+   checkError(err)
+
+   for {
+      conn, err := listener.Accept()
+      if err != nil {
+         continue
+      }
+      handleClient(conn)
+      conn.Close()
+   }
+}    
