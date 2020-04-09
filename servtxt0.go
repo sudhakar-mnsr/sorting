7b6fe0401208a487d39d3803316141f4517a4273
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-09 23:37:15 +0530

    examples

---
+++ servtxt0.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
+	curr "github.com/vladimirvivien/go-networking/currency/lib0"
+)
+
+var currencies = curr.Load("../data.csv")
