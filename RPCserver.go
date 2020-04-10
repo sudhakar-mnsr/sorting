13dc75d832df2680e4deb18d7fcd5e48bbc916bd
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-10 22:56:21 +0530

    examples

--- RPCserver.go
+++ RPCserver.go
@@ -9,3 +9,17 @@ import (
 )
 
 type MyInterface int 
+
+func (t *MyInterface) Add(arguments *sharedRPC.MyInts, reply *int) error {
+   s1 := 1
+   s2 := 1
+   if arguments.S1 == true {
+      s1 = -1
+   }
+   if arguments.S2 == true {
+      s2 = -1
+   }
+
+   *reply = s1 * int(arguments.S1) + s2 * int(arguments.S2)
+   return nil
+}
