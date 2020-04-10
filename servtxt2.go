e4b661a2502f0aa72089fe830b2b4424c6212f55
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-10 22:45:34 +0530

    examples

--- servtxt2.go
+++ servtxt2.go
@@ -150,3 +150,13 @@ func handleConnection(conn net.Conn) {
 		}
 	}
 }
+
+func parseCommand(cmdLine string) (cmd, param string) {
+	parts := strings.Split(cmdLine, " ")
+	if len(parts) != 2 {
+		return "", ""
+	}
+	cmd = strings.TrimSpace(parts[0])
+	param = strings.TrimSpace(parts[1])
+	return
+}
