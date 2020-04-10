f2328fb5f75ed2bd82ce917c6ab47cdc5878f14f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-10 22:59:00 +0530

    examples

--- RPCserver.go
+++ RPCserver.go
@@ -23,3 +23,19 @@ func (t *MyInterface) Add(arguments *sharedRPC.MyInts, reply *int) error {
    *reply = s1 * int(arguments.S1) + s2 * int(arguments.S2)
    return nil
 }
+
+func (t *MyInterface) Sub(arguments *sharedRPC.MyInts, reply *int) error {
+   s1 := 1
+   s2 :=1
+   
+   if arguments.S1 == true {
+      s1 = -1
+   }
+   
+   if arguments.S2 == true {
+      s2 = -1
+   }
+
+   *reply = s1 * int(arguments.A1) - s2 * (arguments.A2)
+   return nil
+}
