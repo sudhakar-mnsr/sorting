ceaa49c7a9635ddeb11c281f9ca011bce0a5e904
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-10 22:41:35 +0530

    examples

--- servtxt2.go
+++ servtxt2.go
@@ -13,3 +13,32 @@ import (
 )
 
 var currencies = curr.Load("../data.csv")
+
+// This program implements a simple currency lookup service
+// over TCP or Unix Data Socket. It loads ISO currency
+// information using package lib (see above) and uses a simple
+// text-based protocol to interact with the client and send
+// the data.
+//
+// Clients send currency search requests as a textual command in the form:
+//
+// GET <currency, country, or code>
+//
+// When the server receives the request, it is parsed and is then used
+// to search the list of currencies. The search result is then printed
+// line-by-line back to the client.
+//
+// Focus:
+// This version of the currency server focuses on implementing a streaming
+// strategy when receiving data from client to avoid dropping data when the
+// request is larger than the internal buffer. This version uses the bufio
+// package to use buffered readers to stream from net.Conn.
+//
+// Testing:
+// Netcat or telnet can be used to test this server by connecting and
+// sending command using the format described above.
+//
+// Usage: server0 [options]
+// options:
+//   -e host endpoint, default ":4040"
+//   -n network protocol [tcp,unix], default "tcp"
