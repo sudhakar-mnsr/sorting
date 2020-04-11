b82677f83ba77bc22fafc0d955df64425a12bd9f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-11 22:02:21 +0530

    examples

--- servjson0.go
+++ servjson0.go
@@ -14,3 +14,26 @@ import (
 var (
 	currencies = curr.Load("./data.csv")
 )
+
+// This program implements a simple currency lookup service
+// over TCP or Unix Data Socket. It loads ISO currency
+// information using package curlib (see above) and makes
+// and serves it using JSON-enocoded data.
+//
+// Clients send currency search requests as JSON objects such
+// as {"Get":"USD"}. The request data is then unmarshalled to Go
+// type curr.CurrencyRequest{Get:"USD"} using the encoding/json
+// package.
+//
+// The request is then used to search the list of
+// currencies. The search result, a []curr.Currency, is marshalled
+// to JSON array of objects and send to the client.
+//
+// IO Streaming:
+// This version of the server highlights the use of IO streaming
+// when using net.Conn to stream data to and from clients.
+//
+// Usage: server [options]
+// options:
+//   -e host endpoint, default ":4040"
+//   -n network protocol [tcp,unix], default "tcp"
