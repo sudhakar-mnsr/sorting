a35e99ba9e28d43a4e89aa00f4702ebcbebef58e
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-12 21:35:06 +0530

    examples

--- servjson4.go
+++ servjson4.go
@@ -13,3 +13,34 @@ import (
 
 var (
 	currencies = curr.Load("../data.csv")
+
+// This program implements a simple currency lookup service
+// over TCP or Unix Data Socket. It loads ISO currency
+// information using package curr (see above) and uses a simple
+// JSON-encode text-based protocol to exchange data with a client.
+//
+// Clients send currency search requests as JSON objects
+// as {"Get":"<currency name,code,or country"}. The request data is
+// then unmarshalled to Go type curr.CurrencyRequest using
+// the encoding/json package.
+//
+// The request is then used to search the list of
+// currencies. The search result, a []curr.Currency, is marshalled
+// as JSON array of objects and sent to the client.
+//
+// Focus:
+// This version of the program highlights the use of the encoding
+// packages to serialize data to/from Go data types to another
+// representation such as JSON.  This version uses the enconding/json
+// package Encoder/Decoder types which are accept an io.Writer and
+// io.Reader respectively.  This means they can be used directly with
+// the io.Conn value.
+//
+// Testing:
+// Netcat can be used for rudimentary testing.  However, use clientjsonX
+// programs functional tests.
+//
+// Usage: server [options]
+// options:
+//   -e host endpoint, default ":4040"
+//   -n network protocol [tcp,unix], default "tcp"
