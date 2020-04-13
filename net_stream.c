fe8bd639c22234d3e9e7ba3d32437685fc65ac4a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-13 23:22:50 +0530

    examples

---
+++ net_stream.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <sys/errno.h>
+
+#include <sys/stream.h>
+#include <sys/stropts.h>
+#include <sys/tihdr.h>
+
+
+extern int errno;
+
+int
+net_open (char *path, int oflags, void *addr, int addrlen)
+{
+	int fd;
+	int flags;
+
+	if ((fd = open (path, oflags)) < 0)
+	{
+		perror ("open");
+		return (-1);
+	}
+	return (fd);
+}
+
+int
+net_bind (int fd, void *addr, int addrlen)
+{
+	struct {
+		struct T_bind_req msg_hdr;
+		char addr[128];
+	} bind_req;
+	struct strbuf ctlbuf;
+	union T_primitives rcvbuf;
+	struct T_error_ack *error_ack;
+	int flags;
+
+	if (addr == NULL || addrlen == 0)
+	{
+		fprintf (stderr, "No address\n");
+		return (-1);
+	}
