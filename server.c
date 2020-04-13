075e4ecddd8f181d3a8e06852f634bcbe65316d9
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-13 23:28:05 +0530

    examples

--- server.c
+++ server.c
@@ -9,3 +9,21 @@
 extern int errno;
 
 #define SERV_TCP_PORT   6000
+
+main()
+{
+	int fd;
+	struct sockaddr_in	my_addr;
+	void			echo_serv (int);
+
+	bzero((char *) &my_addr, sizeof(my_addr));
+	my_addr.sin_family      = AF_INET;
+	my_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+	my_addr.sin_port        = htons(SERV_TCP_PORT);
+
+	fd = net_open ("/dev/udp", O_RDWR);
+	fd = net_bind (fd, &my_addr, sizeof (struct sockaddr_in));
+
+	echo_serv (fd);
+	exit (0);	/* not reached */
+}
