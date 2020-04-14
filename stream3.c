6c3c99d0acd956f21445d4d85708a2e1cc784710
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-14 23:15:32 +0530

    examples

--- stream3.c
+++ stream3.c
@@ -27,3 +27,22 @@ void nop(int);
 
 extern void error(const char *fmt, ...);
 extern void fatal(const char *fmt, ...)
+
+void
+main(int argc, char *argv[])
+{
+     int i, fd, isoutstr;
+#ifdef FCBUG
+     struct sigaction sa;
+
+     /*
+      * If system contains flow-control bug,
+      * install a signal handler for SIGALRM.
+      */
+     sa.sa_handler = nop;
+     sigemptyset(&sa.sa_mask);
+     sa.sa_flags = 0;
+
+     if (sigaction(SIGALRM, &sa, NULL) < 0)
+         fatal("cat: sigaction failed");
+#endif
