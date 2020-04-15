2711d75434b153c071cb1258f5d8cb8a220bc9c6
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-15 22:46:56 +0530

    examples

---
+++ stream5.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stropts.h>
+#include <signal.h>
+
+#define BUFSIZE (64*1024)
+#define RDSIZE  (BUFSIZE/8)
+
+char buf[BUFSIZE];
+int widx, ridx;      /* write and read indices */
+int totwr, totrd;    /* total amounts read and written */
+int flowctl;         /* 1 if flow-controlled, 0 if not */
+int nfc;             /* number of times flow-controlled */
+
+void catreg(int), cattostream(int);
+int doread(int);
+void dowrite(int), finwrite(void);
+void setblock(int), setnonblock(int);
