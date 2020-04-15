6a71f6b5ce76090c6f500137b26e8a65c7c5895f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-15 22:54:24 +0530

    examples

--- stream5.c
+++ stream5.c
@@ -95,3 +95,12 @@ cattostream(int fd)
 
      sigemptyset(&s);
      sigaddset(&s, SIGPOLL);
+
+     /*
+      * Install a signal handler for SIGPOLL.
+      */
+     sa.sa_handler = dowrite;
+     sigemptyset(&sa.sa_mask);
+     sa.sa_flags = 0;
+     if (sigaction(SIGPOLL, &sa, NULL) < 0)
+         fatal("cat: sigaction failed");
