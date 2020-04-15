67b6a83aa1407f0ddd1352275737218305cdbe2a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-15 22:48:12 +0530

    examples

--- stream5.c
+++ stream5.c
@@ -54,3 +54,11 @@ main(int argc, char *argv[])
      isoutstr = isastream(1);
      if (isoutstr == -1)
          isoutstr = 0;
+     /*
+      * Process each file named on the command line.
+      */
+     for (i = 1; i < argc; i++) {
+         if ((fd = open(argv[i], O_RDONLY)) < 0) {
+             error("cat: cannot open %s", argv[i]);
+             continue;
+         }
