4bf83714661bfa40a901571c954adb2f12971152
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-16 23:34:57 +0530

    examples

--- icmpd.c
+++ icmpd.c
@@ -9,3 +9,12 @@ main(int argc, char **argv)
 
 	if (argc != 1)
 		err_quit("usage: icmpd");
+
+	maxi = -1;					/* index into client[] array */
+	for (i = 0; i < FD_SETSIZE; i++)
+		client[i].connfd = -1;	/* -1 indicates available entry */
+	FD_ZERO(&allset);
+
+	fd4 = Socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	FD_SET(fd4, &allset);
+	maxfd = fd4;
