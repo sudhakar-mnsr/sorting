e170026857df78ee091ad57cbe9efbc50cbf7c50
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-16 21:54:41 +0530

    examples

--- stack_growth.go
+++ stack_growth.go
@@ -10,3 +10,17 @@ func main() {
 	s := "HELLO"
 	stackCopy(&s, 0, [size]int{})
 }
+
+// stackCopy recursively runs increasing the size
+// of the stack.
+//go:noinline
+func stackCopy(s *string, c int, a [size]int) {
+	println(c, s, *s)
+
+	c++
+	if c == 10 {
+		return
+	}
+
+	stackCopy(s, c, a)
+}
