78c77e25d027488815e63a9cedaabcd41d46df75
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-16 23:35:26 +0530

    examples

--- icmpd.c
+++ icmpd.c
@@ -18,3 +18,10 @@ main(int argc, char **argv)
 	fd4 = Socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 	FD_SET(fd4, &allset);
 	maxfd = fd4;
+
+
+#ifdef	IPV6
+	fd6 = Socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
+	FD_SET(fd6, &allset);
+	maxfd = max(maxfd, fd6);
+#endif
