401621c8281ef910640d8fb7f1f62d97fb4a8f41
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-17 23:00:13 +0530

    examples

--- poll.c
+++ poll.c
@@ -9,3 +9,8 @@ comm(int tfd, int nfd)
      int n, i;
      struct pollfd pfd[2];
      char buf[256];
+     pfd[0].fd = tfd;    /* terminal */
+     pfd[0].events = POLLIN;
+     pfd[1].fd = nfd;    /* network */
+     pfd[1].events = POLLIN;
+     for (;;) {
