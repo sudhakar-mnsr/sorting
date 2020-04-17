c259ccdaaa52a3203fd84873fd4f87a92717c3db
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-17 23:04:50 +0530

    examples

--- pipe.c
+++ pipe.c
@@ -14,3 +14,17 @@ chgterm()
       */
      if ((tty = ttyname(0)) == NULL)
          return(-1);
+     /*
+      * Create a pipe and mount one end on top of
+      * the terminal’s device node.  Then change
+      * the mode of the pipe to give it the same
+      * permissions as terminals.
+      */
+     if (pipe(pfd) < 0)
+         return(-1);
+     if ((fattach(pfd[1], tty) < 0) ||
+       (chmod(tty, TTYMODE) < 0)) {
+         close(pfd[0]);
+         close(pfd[1]);
+         return(-1);
+     }
