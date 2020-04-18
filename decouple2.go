f87768ffc04348eb0afdccf3229cd42743560d9a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-18 22:21:20 +0530

    examples

---
+++ decouple2.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+   "errors"
+   "fmt"
+   "io"
+   "math/rand"
+   "time"
+)
+
+func init() {
+   rand.Seed(time.Now().UnixNano())
+}
+
+// Data is the structure of the data we are copying
+type Data struct {
+   Line string
+}
+
+type Puller interface {
+   Pull(d *Data) error
+}
+
+type Storer interface {
+   Store(d *Data) error
+}
+
+type PullStorer interface {
+   Puller
+   Storer
+}
+
+// Xenia is a system we need to pull data from.
+type Xenia struct {
+   Host string
+   Timeout time.Duration
+}
+
+// PUll knows how to pull data out of Xenia.
+func (*Xenia) Pull(d *Data) error {
+   switch rand.Intn(10) {
+   case 1,9:
+      return io.EOF
+   case 5:
+      return errors.New("Error reading data from Xenia")
+   default:
+      d.Line = "Data"
+      fmt.Println("In:", d.Line)
+      return nil
+   }
+} 
+
+// Pillar is a system we need to store data into.
+type Pillar struct {
+   Host string
+   Timeout time.Duration
+}
+
+// Store knows how to store data into Pillar
+func (*Pillar) Store(d *Data) error {
+   fmt.Println("Out:", d.Line)
+   return nil
+}
+
+// System wraps Xenia and Pillar together into a single system
+type System struct {
+   Xenia
+   Pillar
+}
+
+// Pull knows how to pull bulks of data from Xenia
+func pull(p Puller, data []Data) (int, error) {
+   for i := range data {
+      if err := p.Pull(&data[i]); err != nil {
+         return i, err
+      }
+   }
+   return len(data), nil
+}
+
+// store knows how to store bulks of data into Pillar
+func store(s Storer, data []Data) (int, error) {
+   for i := range data {
+      if err := s.Store(&data[i]); err !=nil {
+         return i, err
+      }
+   }
+   return len(data), nil
+}
+
+func Copy(ps PullStorer, batch int) error {
+   data := make([]Data, batch)
+   for {
+      i, err := pull(ps, data)
+      if i > 0 {
+         if _, err := store(ps, data[:i]); err != nil {
+            return err
+         }
+      }
+
+      if err != nil {
+         return err
+      }
+   }
+}
+
+func main() {
+   sys := System{
+             Xenia: Xenia{
+                    Host: "localhost:8000",
+                    Timeout: time.Second,
+             },
+             Pillar: Pillar{
+                    Host: "localhost:9000",
+                    Timeout: time.Second,
+             },
+   }
+   if err := Copy(&sys, 3); err != io.EOF {
+      fmt.Println(err)
+   }
+} 
