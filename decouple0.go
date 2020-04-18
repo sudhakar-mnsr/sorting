5daed29663ed1ed83ac20240908d837a53345791
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-18 14:54:43 +0530

    examples

--- decouple0.go
+++ decouple0.go
@@ -64,3 +64,13 @@ func pull(x *Xenia, data []Data) (int, error) {
    }
    return len(data), nil
 }
+
+// store knows how to store bulks of data into Pillar
+func store(p *Pillar, data []Data) (int, error) {
+   for i := range data {
+      if err := p.Store(&data[i]); err !=nil {
+         return i, err
+      }
+   }
+   return len(data), nil
+}
