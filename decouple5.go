a277cb0e170fac4db21ac206ef4d3045d8350701
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-18 22:40:24 +0530

    examples

--- decouple5.go
+++ decouple5.go
@@ -94,16 +94,16 @@ func Copy(sys *System, batch int) error {
 }
 
 func main() {
-   sys := System{
-             Puller: &Xenia{
-                    Host: "localhost:8000",
-                    Timeout: time.Second,
-             },
-             Storer: &Pillar{
-                    Host: "localhost:9000",
-                    Timeout: time.Second,
-             },
+   Puller: &Xenia{
+      Host: "localhost:8000",
+      Timeout: time.Second,
    }
+
+   Storer: &Pillar{
+      Host: "localhost:9000",
+      Timeout: time.Second,
+   }
+
    if err := Copy(&sys, 3); err != io.EOF {
       fmt.Println(err)
    }
