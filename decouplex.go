31c75539e566104e4c1397269ad26871a0f8a18a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-19 23:59:59 +0530

    examples

--- decouplex.go
+++ decouplex.go
@@ -12,7 +12,6 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-// =============================================================================
 
 // Data is the structure of the data we are copying.
 type Data struct {
