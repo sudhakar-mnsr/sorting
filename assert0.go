fad7e79fd935fc5b8d0d1c09a7b908d2f3ced6ae
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-20 16:40:02 +0530

    examples

--- assert0.go
+++ assert0.go
@@ -12,3 +12,17 @@ type Locker interface {
    Lock()
    Unlock()
 }
+
+// MoveLocker provides support for moving and locking things.
+type MoveLocker interface {
+   Mover
+   Locker
+}
+
+// bike represents a concrete type for the example
+type bike struct{}
+
+// Move can change the position of a bike
+func (bike) Move() {
+   fmt.Println("Moving the bike")
+}
