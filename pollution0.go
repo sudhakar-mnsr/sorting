f76b7a14d4875ab91590437f2c72e911a8b32f8c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-20 17:25:31 +0530

    examples

--- pollution0.go
+++ pollution0.go
@@ -49,3 +49,12 @@ func main() {
    srv.Stop()
    srv.Wait()
 }
+
+// Smells
+// * The package declares an interface that matches the entire API of 
+// * its own concrete type
+// * The interface is exported but the concrete type is unexported.
+// * The factory function returns the interface value with the unexported
+// * concrete type value inside.
+// * The interface can be removed and nothing changes for the user of API
+// * The interface is not decoupleing the API from change.
