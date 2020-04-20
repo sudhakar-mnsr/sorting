d4d1c62d6964bbf77ba3c0d9c452abb50b37604b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-20 16:57:49 +0530

    examples

---
+++ assert3.go
@@ -0,0 +1,10 @@
+// Sample program to show how method sets can affect behavior.
+package main
+
+import "fmt"
+
+// user defines a user in the system.
+type user struct {
+	name  string
+	email string
+}
