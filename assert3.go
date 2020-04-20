1f854d780ac4f3080515e5bf48a83a2d87a932a7
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-20 17:11:23 +0530

    examples

--- assert3.go
+++ assert3.go
@@ -8,3 +8,19 @@ type user struct {
 	name  string
 	email string
 }
+
+// String implements the fmt.Stringer interface.
+func (u *user) String() string {
+   return fmt.Sprintf("My name is %q and my email is %q", u.name, u.email)
+}
+
+func main() {
+   // Create a value of type user
+   u := user{
+        name: "Bill",
+        email: "bill@ardanlabs.com",
+   }
+
+   fmt.Println(u)
+   fmt.Println(&u)
+} 
