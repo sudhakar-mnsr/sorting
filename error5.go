985bb9fc231ded51722beeb0ab4275bfd919214a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-21 23:30:32 +0530

    examples

--- error5.go
+++ error5.go
@@ -16,3 +16,15 @@ type AppError struct {
 func (c *AppError) Error() string {
 	return fmt.Sprintf("App Error, State: %d", c.State)
 }
+
+func main() {
+
+	// Make the function call and validate the error.
+	if err := firstCall(10); err != nil {
+
+		// Use type as context to determine cause.
+		switch v := errors.Cause(err).(type) {
+		case *AppError:
+
+			// We got our custom error type.
+			fmt.Println("Custom App Error:", v.State)
