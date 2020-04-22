a54e9040f65e751d378f70642cb3a2cd6a8e275a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-22 22:52:21 +0530

    examples

--- error4.go
+++ error4.go
@@ -65,3 +65,9 @@ func (c *client) BehaviorAsContext() {
 	for {
 		line, err := c.reader.ReadString('\n')
 		if err != nil {
+			switch e := err.(type) {
+			case temporary:
+				if !e.Temporary() {
+					log.Println("Temporary: Client leaving chat")
+					return
+				}
