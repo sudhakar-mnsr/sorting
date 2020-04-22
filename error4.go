9f5310b57aed332ced4f3202f78a982eab8d13e9
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-22 22:49:31 +0530

    examples

--- error4.go
+++ error4.go
@@ -21,3 +21,10 @@ func (c *client) TypeAsContext() {
 	for {
 		line, err := c.reader.ReadString('\n')
 		if err != nil {
+
+			switch e := err.(type) {
+			case *net.OpError:
+				if !e.Temporary() {
+					log.Println("Temporary: Client leaving chat")
+					return
+				}
