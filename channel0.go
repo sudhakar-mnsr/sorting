67d9c7b8620464708863bb8e65aaa800da05d385
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-23 23:37:58 +0530

    examples

--- channel0.go
+++ channel0.go
@@ -254,3 +254,19 @@ func drop() {
 	time.Sleep(time.Second)
 	fmt.Println("-------------------------------------------------------------")
 }
+
+
+// cancellation: You are a manager and you hire a new employee. Your new
+// employee knows immediately what they are expected to do and starts their
+// work. You sit waiting for the result of the employee's work. The amount
+// of time you wait on the employee is unknown because you need a
+// guarantee that the result sent by the employee is received by you. Except
+// you are not willing to wait forever for the employee to finish their work.
+// They have a specified amount of time and if they are not done, you don't
+// wait and walk away.
+func cancellation() {
+	duration := 150 * time.Millisecond
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	defer cancel()
+
+	ch := make(chan string, 1)
