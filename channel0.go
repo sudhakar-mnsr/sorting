38cb4aa84867460af03995dbd1f8d4ba6f938493
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-23 23:33:04 +0530

    examples

--- channel0.go
+++ channel0.go
@@ -59,3 +59,22 @@ func waitForResult() {
 	time.Sleep(time.Second)
 	fmt.Println("-------------------------------------------------------------")
 }
+
+// fanOut: You are a manager and you hire one new employee for the exact amount
+// of work you have to get done. Each new employee knows immediately what they
+// are expected to do and starts their work. You sit waiting for all the results
+// of the employees work. The amount of time you wait on the employees is
+// unknown because you need a guarantee that all the results sent by employees
+// are received by you. No given employee needs an immediate guarantee that you
+// received their result.
+func fanOut() {
+	emps := 2000
+	ch := make(chan string, emps)
+
+	for e := 0; e < emps; e++ {
+		go func(emp int) {
+			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+			ch <- "paper"
+			fmt.Println("employee : sent signal :", emp)
+		}(e)
+	}
