76fc812465959b2f107338f6a565a576bd95c249
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-23 23:32:05 +0530

    examples

--- channel0.go
+++ channel0.go
@@ -14,3 +14,31 @@ import (
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
+
+func main() {
+
+	waitForResult()
+	// fanOut()
+
+	// waitForTask()
+	// pooling()
+
+	// Advanced patterns
+	// fanOutSem()
+	// boundedWorkPooling()
+	// drop()
+
+	// Cancellation Pattern
+	// cancellation()
+
+	// Retry Pattern
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
+	// retryTimeout(ctx, time.Second, func(ctx context.Context) error { return errors.New("always fail") })
+}
+
+// waitForResult: You are a manager and you hire a new employee. Your new
+// employee knows immediately what they are expected to do and starts their
+// work. You sit waiting for the result of the employee's work. The amount
+// of time you wait on the employee is unknown because you need a
+// guarantee that the result sent by the employee is received by you.
