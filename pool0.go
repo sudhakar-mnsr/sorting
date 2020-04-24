7510c2efd37622d09f5575b3cfe6139e5bc30984
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-24 22:22:34 +0530

    examples

---
+++ pool0.go
@@ -0,0 +1,19 @@
+// This sample program demonstrates how to use the pool package
+// to share a simulated set of database connections.
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/ardanlabs/gotraining/topics/go/concurrency/patterns/pool"
+)
+
+const (
+	maxGoroutines = 25 // the number of routines to use.
+	numPooled     = 2  // number of resources in the pool
+)
