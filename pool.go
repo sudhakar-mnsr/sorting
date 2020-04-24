2a65898a52c643391f09ed2c9615a075855d3c38
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-24 22:19:00 +0530

    examples

--- pool.go
+++ pool.go
@@ -25,3 +25,14 @@ var ErrPoolClosed = errors.New("Pool has been closed")
 // New creates a pool that manages resources. A pool requires a
 // function that can allocate a new resource and the size of
 // the pool.
+
+func New(size uint, f func() (io.Closer, error)) (*Pool, error) {
+	if size == 0 {
+		return nil, errors.New("Size value too small")
+	}
+
+	return &Pool{
+		factory:   f,
+		resources: make(chan io.Closer, size),
+	}, nil
+}
