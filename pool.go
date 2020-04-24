ee7c17071184a8605d6b79038215eaa109a5ea6c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-24 22:21:10 +0530

    examples

--- pool.go
+++ pool.go
@@ -86,3 +86,12 @@ func (p *Pool) Close() error {
 	// Secure this operation with the Release operation.
 	p.mu.Lock()
 	defer p.mu.Unlock()
+
+
+	// If the pool is already close, don't do anything.
+	if p.closed {
+		return ErrPoolClosed
+	}
+
+	// Set the pool as closed.
+	p.closed = true
