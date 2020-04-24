93615bfdffec183cf09aa89679801b20841b1c26
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-25 04:27:01 +0530

    examples

--- debug/stack_trace0.go
+++ debug/stack_trace0.go
@@ -1,11 +1,17 @@
 // Sample program to show how to read a stack trace
 package main
 
+import (
+   "fmt"
+   "runtime/debug"
+)
+
 func main() {
    example(make([]string, 2,4), "hello", 10)
 }
 
 func example(slice[]string, str string, i int) {
-   panic("Want stack trace")
+//   panic("Want stack trace")
+   fmt.Println("Stacktrace from Panic\n" + string(debug.Stack()))
 }
 
