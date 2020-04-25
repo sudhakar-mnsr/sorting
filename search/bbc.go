ab4a7821179e3f6077125fc31af8f530551d7f50
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-25 22:57:23 +0530

    examples

---
+++ search/bbc.go
@@ -0,0 +1,13 @@
+package search
+
+import "log"
+
+var bbcFeeds = []string{
+	"http://feeds.bbci.co.uk/news/rss.xml",
+	"http://feeds.bbci.co.uk/news/world/rss.xml",
+	"http://feeds.bbci.co.uk/news/politics/rss.xml",
+	"http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml",
+}
+
+// BBC provides support for BBC searches.
+type BBC struct{}
