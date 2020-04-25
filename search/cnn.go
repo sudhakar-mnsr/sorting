ace4a0b2b2732f7ab554a5984d0c856fdb88df5b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-25 22:58:47 +0530

    examples

---
+++ search/cnn.go
@@ -0,0 +1,13 @@
+package search
+
+import "log"
+
+var cnnFeeds = []string{
+	"http://rss.cnn.com/rss/cnn_topstories.rss",
+	"http://rss.cnn.com/rss/cnn_world.rss",
+	"http://rss.cnn.com/rss/cnn_us.rss",
+	"http://rss.cnn.com/rss/cnn_allpolitics.rss",
+}
+
+// CNN provides support for CNN searches.
+type CNN struct{}
