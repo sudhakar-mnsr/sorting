84a807a3ded56d588cfd13c41fd0b0fbe3915d0c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-26 21:45:22 +0530

    examples

--- stack_trace/example2.go
+++ stack_trace/example2.go
@@ -9,3 +9,12 @@ func main() {
 func example(b1, b2, b3 bool, i uint8) {
 	panic("Want stack trace")
 }
+
+/*
+	panic: Want stack trace
+	goroutine 1 [running]:
+	main.example(0xc019010001)
+		stack_trace/example2/example2.go:13 +0x39
+	main.main()
+		stack_trace/example2/example2.go:8 +0x29
+--------------------------------------------------------------------------------
