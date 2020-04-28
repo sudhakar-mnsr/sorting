9469fd32b0182a3131bd2836466c704c00cc09d0
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-28 22:12:21 +0530

    examples

--- trace/trace.go
+++ trace/trace.go
@@ -57,3 +57,14 @@ func main() {
 
 	log.Printf("Searching %d files, found %s %d times.", len(docs), topic, n)
 }
+
+func freq(topic string, docs []string) int {
+	var found int
+
+	for _, doc := range docs {
+		file := fmt.Sprintf("%s.xml", doc[:8])
+		f, err := os.OpenFile(file, os.O_RDONLY, 0)
+		if err != nil {
+			log.Printf("Opening Document [%s] : ERROR : %v", doc, err)
+			return 0
+		}
