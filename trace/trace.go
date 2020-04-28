fa776e1307cf1aeb46baafbf8020bbd9816f7dd9
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-28 22:11:24 +0530

    examples

--- trace/trace.go
+++ trace/trace.go
@@ -15,3 +15,21 @@ import (
 	"sync"
 	"sync/atomic"
 )
+
+type (
+	item struct {
+		XMLName     xml.Name `xml:"item"`
+		Title       string   `xml:"title"`
+		Description string   `xml:"description"`
+	}
+
+	channel struct {
+		XMLName xml.Name `xml:"channel"`
+		Items   []item   `xml:"item"`
+	}
+
+	document struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel channel  `xml:"channel"`
+	}
+)
