2bff589b64c71afd00470f7b86879c2ff2c503a4
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-30 17:54:58 +0530

    examples

--- service/templates.go
+++ service/templates.go
@@ -12,3 +12,13 @@ import (
 
 // views contains a map of templates for rendering views.
 var views = make(map[string]*template.Template)
+
+// init loads the existing templates for use by routing code.
+func init() {
+	// In order for the endpoint tests to run this needs to be
+	// physically located. Trying to avoid configuration for now.
+	pwd, _ := os.Getwd()
+	loadTemplate("layout", pwd+"/views/basic-layout.html")
+	loadTemplate("search", pwd+"/views/search.html")
+	loadTemplate("results", pwd+"/views/results.html")
+}
