fdf728d18b3b1a5518235a49c212b8f1ccdaed16
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-30 17:51:57 +0530

    examples

--- service/handler.go
+++ service/handler.go
@@ -43,3 +43,10 @@ func formValues(r *http.Request) (map[string]interface{}, search.Options) {
 
 	fv["term"] = r.FormValue("term")
 	options.Term = r.FormValue("term")
+	if r.FormValue("cnn") == "on" {
+		fv["cnn"] = "checked"
+		options.CNN = true
+	} else {
+		fv["cnn"] = ""
+	}
+
