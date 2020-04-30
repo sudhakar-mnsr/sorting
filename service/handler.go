5a1b04e3d65bcd4763e4d1ad0e0c410dec6eb536
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-04-30 17:51:02 +0530

    examples

--- service/handler.go
+++ service/handler.go
@@ -12,3 +12,13 @@ import (
 
 // req keeps track of the number of requests.
 var req = expvar.NewInt("requests")
+
+// handler handles the search route processing.
+func handler(w http.ResponseWriter, r *http.Request) {
+	uid := uuid.New()
+
+	// Add a new counter for monitoring.
+	req.Add(1)
+
+	// Capture all the form values.
+	fv, options := formValues(r)
