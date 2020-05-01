8a2c763991318028d04cc49fba2de547493cfe40
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-01 22:52:16 +0530

    examples

---
+++ memcpu/stream.go
@@ -0,0 +1,13 @@
+// Sample program that takes a stream of bytes and looks for the bytes
+// “elvis” and when they are found, replace them with “Elvis”. The code
+// cannot assume that there are any line feeds or other delimiters in the
+// stream and the code must assume that the stream is of any arbitrary length.
+// The solution cannot meaningfully buffer to the end of the stream and
+// then process the replacement.
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
