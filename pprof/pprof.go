7e22d9d0e1ff12927c0a920ac6376fc43aff9068
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-02 19:36:27 +0530

    examples

--- pprof/pprof.go
+++ pprof/pprof.go
@@ -15,3 +15,18 @@ func main() {
 	log.Println("listener : Started : Listening on: http://localhost:4000")
 	http.ListenAndServe(":4000", nil)
 }
+
+// sendJSON returns a simple JSON document.
+func sendJSON(rw http.ResponseWriter, r *http.Request) {
+	u := struct {
+		Name  string
+		Email string
+	}{
+		Name:  "bill",
+		Email: "bill@ardanlabs.com",
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(200)
+	json.NewEncoder(rw).Encode(&u)
+}
