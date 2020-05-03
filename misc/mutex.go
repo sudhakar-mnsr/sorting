d8693744438897c945c766d5dd02bec0864d4148
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-03 23:47:10 +0530

    examples

--- misc/mutex.go
+++ misc/mutex.go
@@ -7,3 +7,11 @@ import (
 	"testing"
 	"time"
 )
+
+var (
+	// data is a slice that will be shared.
+	data = make([]string, 1000)
+
+	// rwMutex is used to define a critical section of code.
+	rwMutex sync.RWMutex
+)
