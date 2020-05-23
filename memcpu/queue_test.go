54d519659846855fca620e5f3c7ee1cd022262e6
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-05-23 15:35:28 +0530

    examples

---
+++ memcpu/queue_test.go
@@ -0,0 +1,8 @@
+/*
+	// This is the API you need to build for these tests. You will need to
+	// change the import path in this test to point to your code.
+	package queue
+	// Data represents what is being stored on the queue.
+	type Data struct {
+		Name string
+	}
